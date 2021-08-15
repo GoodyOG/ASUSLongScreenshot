6bcd352f64d98f82f068a4c5acf2995e246d28a7
luisbocanegra <luisbocanegra17b@gmail.com>
2021-08-15 03:52:35 -0500

    Maximun supported version is Android 11

--- install.sh
+++ install.sh
@@ -113,11 +113,11 @@ REPLACE="
 # Set what you want to display when installing your module
 
 print_modname() {
-  ui_print "***********************************"
-  ui_print "* ASUS Long Screenshot port       *"
-  ui_print "* Support: Android 9+             *"
-  ui_print "* By: @luisbocanegra              *"
-  ui_print "***********************************"
+  ui_print "*********************************"
+  ui_print "* ASUS Long Screenshot port     *"
+  ui_print "* Support: Android 9-11         *"
+  ui_print "* By: @luisbocanegra            *"
+  ui_print "*********************************"
 }
 
 # Copy/extract your module files into $MODPATH in on_install.
@@ -126,7 +126,7 @@ on_install() {
   # The following is the default implementation: extract $ZIPFILE/system to $MODPATH
   # Extend/change the logic to whatever you want
   
-  if [ $API -lt 28 ]; then
+  if [ $API -lt 28 ] && [ $API -gt 30 ]; then
   abort "Error: API $API is not supported :("
   fi
   ui_print "- Extracting module files"
